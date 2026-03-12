955059b1accf2247b4a06e2839ad8ce6ab130ac8
lucas-maes <lucas.maes7711@gmail.com>
2026-03-11 20:06:54 -0400

    revert lazzy import

--- stable_worldmodel/__init__.py
+++ stable_worldmodel/__init__.py
@@ -1,27 +1,18 @@
-import importlib
+from stable_worldmodel import data, envs, policy, solver, spaces, utils, wm, wrapper
+from stable_worldmodel.world import World
+from stable_worldmodel.policy import PlanConfig
+from stable_worldmodel.utils import pretraining
 
-_lazy: dict[str, tuple[str, str | None]] = {
-    'World': ('stable_worldmodel.world', 'World'),
-    'PlanConfig': ('stable_worldmodel.policy', 'PlanConfig'),
-    'pretraining': ('stable_worldmodel.utils', 'pretraining'),
-    'data': ('stable_worldmodel.data', None),
-    'envs': ('stable_worldmodel.envs', None),
-    'policy': ('stable_worldmodel.policy', None),
-    'solver': ('stable_worldmodel.solver', None),
-    'spaces': ('stable_worldmodel.spaces', None),
-    'utils': ('stable_worldmodel.utils', None),
-    'wm': ('stable_worldmodel.wm', None),
-    'wrapper': ('stable_worldmodel.wrapper', None),
-}
-
-__all__ = list(_lazy)
-
-
-def __getattr__(name: str):
-    if name in _lazy:
-        module_path, attr = _lazy[name]
-        mod = importlib.import_module(module_path)
-        val = getattr(mod, attr) if attr else mod
-        globals()[name] = val
-        return val
-    raise AttributeError(f"module 'stable_worldmodel' has no attribute {name!r}")
+__all__ = [
+    'World',
+    'PlanConfig',
+    'pretraining',
+    'data',
+    'envs',
+    'policy',
+    'solver',
+    'spaces',
+    'utils',
+    'wm',
+    'wrapper',
+]
